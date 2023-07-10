d23ebfd096637b54041e324d9c117682628db35f
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2023-07-10 20:35:11 +0700

    up

---
+++ index.js
@@ -0,0 +1,8 @@
+// console.log(window);
+// window object
+// is the global scope in JavaScript, it contains all built-in functions and variables that are available to
+// every other part of a web page or application
+
+require('./coba.js');
+
+console.log('hello world');
